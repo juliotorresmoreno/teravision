b4ec8b29ab1502bd4bc8fc0cfed3f9f21625cb70
Julio Torres <jtorres990@gmail.com>
2020-02-20 17:46:52 -0500

    insertion endpoint added

--- models/user.go
+++ models/user.go
@@ -4,10 +4,15 @@ import "time"
 
 // User model of users
 type User struct {
-	ID   uint64    `xorm:"id integer pk"`
-	Name string    `xorm:"name varchar(100) not null"`  // ○ Nombre Completo (Requerido)
-	DNI  string    `xorm:"dni varchar(20) not null uk"` // ○ DNI o cédula (Requerido)
-	date time.Time `xorm:"date time with time zone"`    // ○ Fecha de Nacimiento. (El usuario puede o no enviar esta información)
+	ID   uint      `json:"id" xorm:"bigint not null autoincr pk"`
+	Name string    `json:"name" xorm:"name varchar(100) not null"`      // ○ Nombre Completo (Requerido)
+	DNI  string    `json:"dni"  xorm:"dni varchar(20) not null unique"` // ○ DNI o cédula (Requerido)
+	Date time.Time `json:"date" xorm:"date time with time zone"`        // ○ Fecha de Nacimiento. (El usuario puede o no enviar esta información)
+}
+
+// CleanForInsert clean for insert
+func (that *User) CleanForInsert() {
+	that.ID = 0
 }
 
 // TableName name in bd
